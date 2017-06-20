5b5ace2dddc48278ee03e3109e6b06aedce6e15e
jingpengwu <jingpeng.wu@gmail.com>
2017-06-20 14:18:42 -0400

    remove get_config_dict function; modules seems can not share function names

--- src/GSDicts.jl
+++ src/GSDicts.jl
@@ -1,11 +1,10 @@
-__precompile__(true)
+#__precompile__(true)
 
 module GSDicts
 
 using GoogleCloud
 using GoogleCloud.Utils.Storage
 using JSON
-#import BigArrays: get_config_dict, NoSuchKeyException
 
 include("types.jl")
 include("base.jl")
