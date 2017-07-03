f27f2740f104443ba3c998775f7d0445bb2d5725
jingpengwu <jingpeng.wu@gmail.com>
2017-07-03 10:01:33 -0400

    fix << error

--- src/GSDicts.jl
+++ src/GSDicts.jl
@@ -5,10 +5,7 @@ module GSDicts
 using GoogleCloud
 using GoogleCloud.Utils.Storage
 using JSON
-<<<<<<< HEAD
 import BigArrays: NoSuchKeyException
-=======
->>>>>>> 5b5ace2dddc48278ee03e3109e6b06aedce6e15e
 
 include("types.jl")
 include("base.jl")
